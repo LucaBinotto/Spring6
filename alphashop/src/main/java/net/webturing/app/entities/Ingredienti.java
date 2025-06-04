c31de6ba0c13b7897149c3a951a7d7488fa68416
LucaBinotto <brising@libero.it>
2025-06-04 17:06:53 +0200

    relazione @onetoone

--- alphashop/src/main/java/net/webturing/app/entities/Ingredienti.java
+++ alphashop/src/main/java/net/webturing/app/entities/Ingredienti.java
@@ -1,8 +1,11 @@
 package net.webturing.app.entities;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.PrimaryKeyJoinColumn;
 import jakarta.persistence.Table;
 
 @Entity
@@ -11,8 +14,13 @@ public class Ingredienti {
 	
 	@Id
 	@Column(name="codart")
+	@OneToOne
 	private String codArt;
 	@Column(name="info")
 	private String info;
+	
+	@OneToOne
+	@PrimaryKeyJoinColumn
+	private Articolo articolo;
 
 }

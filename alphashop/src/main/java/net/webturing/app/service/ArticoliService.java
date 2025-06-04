3c19b2e15d842e94f4270d142162f61bb1e3adf8
LucaBinotto <brising@libero.it>
2025-06-04 19:10:25 +0200

    added service level

--- alphashop/src/main/java/net/webturing/app/service/ArticoliService.java
+++ alphashop/src/main/java/net/webturing/app/service/ArticoliService.java
@@ -6,5 +6,11 @@ import net.webturing.app.entities.Articolo;
 
 public interface ArticoliService {
 
-	public List<Articolo> SelAll();
+	public List<Articolo> selAll();
+	
+	public Articolo selByCodArt(String codart);
+	
+	public List<Articolo> selByDescrizione(String filter, int page, int numrec);
+	
+	public Articolo selByBarcode(String barcode);
 }
--- alphashop/src/main/java/net/webturing/app/service/ArticoliServiceImpl.java
+++ alphashop/src/main/java/net/webturing/app/service/ArticoliServiceImpl.java
@@ -1,30 +1,44 @@
 package net.webturing.app.service;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import net.webturing.app.entities.Articolo;
+import net.webturing.app.repository.ArticoliRepository;
 
 @Service
 public class ArticoliServiceImpl implements ArticoliService{
 	
-	private static List<Articolo> getArticoli(){
-		List<Articolo> articoli = new ArrayList<>();
-		
-		articoli.add(new Articolo("014600301","BARILLA FARINA 1 KG","PZ",24,1,1.09));
-		articoli.add(new Articolo("013500121","BARILLA PASTA GR.500 N.70 1/2 PENNE","PZ",30,0.5,1.39));
-		articoli.add(new Articolo("007686402","FINDUS FIOR DI NASELLO 300 GR","PZ",8,0.3,4.59));
-		articoli.add(new Articolo("057549001","FINDUS CROCCOLE 400 GR","PZ",12,0.4,3.99));
-		
-		return articoli;
-	}
-	
+	@Autowired
+	ArticoliRepository artRepo;
+
 	@Override
-	public List<Articolo> SelAll() {
+	public List<Articolo> selAll() {
 		
-		return getArticoli();
+		return artRepo.findAll();
+	}
+
+	@Override
+	public Articolo selByCodArt(String codart) {
+		
+		return artRepo.findByCodArt(codart);
+	}
+
+	@Override
+	public List<Articolo> selByDescrizione(String filter, int page, int numrec) {
+		
+		filter = "%".concat(filter.toUpperCase().concat("%"));
+		Pageable pageAndRecords = PageRequest.of(page, numrec);
+		return artRepo.findByDescrizioneLike(filter, pageAndRecords);
+	}
+
+	@Override
+	public Articolo selByBarcode(String barcode) {
+		return artRepo.selByEan(barcode);
 	}
 
 }
